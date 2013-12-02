5bc89545d3be2110556e7f75ada9286b1e6db2d6
Fernando González López - Peñalver <alu0100256543@ull.edu.es>
2013-12-02 20:53:05 +0000

    Actualizado a la ultima versión de PhpUnit

--- test/PictureTest.php
+++ test/PictureTest.php
@@ -1,15 +1,16 @@
 <?php
 
 require_once "./clases/Picture.php";
-require_once 'PHPUnit.php';
 
-class PictureTest extends PHPUnit_TestCase{
+class PictureTest extends PHPUnit_Framework_TestCase{
     
     var $_picture;
     
     function setUp(){
         $this->_picture = new Picture(0, "user", "./images/Big Ben.JPG", 1.0, -0.57);
     }
+    
+    public function tearDown(){ }
     
     function testValidPath(){
         $this->assertTrue($this->_picture->validPath() == true);
--- test/runTests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once './test/PictureTest.php';
-require_once 'PHPUnit.php';
-
-$suite = new PHPUnit_TestSuite("PictureTest");
-$result = PHPUnit::run($suite);
-
-echo $result->toString();
